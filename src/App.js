995a121ffb83db50160ba1adc7cc99bea0b24425
beomseok <abgh012347@gmail.com>
2025-02-14 13:11:10 +0900

    로그인 및 회원가입 페이지 관련 Route 처리 및 명칭 변경

--- src/App.js
+++ src/App.js
@@ -6,14 +6,21 @@ import Header from "./components/Share/Header";
 import Footer from "./components/Share/Footer";
 import MainPages from "./pages/MainPages";
 import Kakao from './components/Share/Kakao';
-import LoginPage from "./pages/Login";
-import JoinForm from "./pages/Join";
+import LoginPage from "./pages/Login/Login";
+import JoinSelectAuthorization from "./pages/Login/JoinSelectAuthorization";
+import JoinUser from "./pages/Login/JoinUser";
 import KakaoRedirectPage from "./pages/KakaoRedirectPage";
 import NaverRedirectPage from "./pages/NaverRedirectPage";
 import GoogleRedirectPage from "./pages/GoogleRedirectPage";
 import Ranking from "./pages/RankingPages";
 import Information from "./components/info/Information";
 import InfoDetailPage from "./pages/InfoDetailPage";
+import FindAccountId from "./pages/Login/FindAccountId";
+import FindAccountPassword from "./pages/Login/FindAccountPassword";
+import FindAccountPasswordDetail from "./pages/Login/FindAccountPasswordDetail";
+import JoinTeam from "./pages/Login/JoinTeam";
+import TeamAgree from "./pages/Login/TeamAgree";
+import UserAgree from "./pages/Login/UserAgree";
 function App() {
 
     return (
@@ -23,13 +30,24 @@ function App() {
                 <Route path="/" element={<MainPages />} />
 
                 <Route path="/login" element={<LoginPage />} />
-                <Route path="/register" element={<JoinForm />} />
+                <Route path="/register" element={<JoinSelectAuthorization />} />
+                <Route path="/register/JoinUser" element={<JoinUser />} />
+                <Route path="/register/JoinTeam" element={<JoinTeam />} />
+                <Route path="/register/AgreeUser" element={<UserAgree />} />
+                <Route path="/register/AgreeTeam" element={<TeamAgree />} />
                 <Route path="/member/kakao" element={<KakaoRedirectPage />} />
                 <Route path="/member/naver" element={<NaverRedirectPage />} />
                 <Route path="/member/google" element={<GoogleRedirectPage />} />
                 <Route path="/ranking" element={<Ranking />}/>
 
+
+
                 <Route path="/infoDetail/:festivalId" element={<InfoDetailPage />} />
+
+                {/*  계정 찾기  */}
+                <Route path="/account/findId" element={<FindAccountId />} />
+                <Route path="/account/findPassword" element={<FindAccountPassword />} />
+                <Route path="/account/findPassword/detail" element={<FindAccountPasswordDetail />} />
 
             </Routes>
 
