7c3f9d99ef3feafd040808d2e3ec8d9923c91b0b
Heeyeon An <gmldus4613@naver.com>
2025-02-28 20:48:37 +0900

    firebase 수정

--- src/config/FirebaseConfig.js
+++ src/config/FirebaseConfig.js
@@ -2,11 +2,25 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: String(window.env.REACT_APP_FIREBASE_API_KEY),
-  authDomain: String(window.env.REACT_APP_FIREBASE_AUTH_DOMAIN),
-  projectId: String(window.env.REACT_APP_FIREBASE_PROJECT_ID),
-  messagingSenderId: String(window.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID),
-  appId: String(window.env.REACT_APP_FIREBASE_APP_ID),
+  apiKey: JSON.stringify(window.env.REACT_APP_FIREBASE_API_KEY).replace(
+    /(^"|"$)/g,
+    ""
+  ),
+  authDomain: JSON.stringify(window.env.REACT_APP_FIREBASE_AUTH_DOMAIN).replace(
+    /(^"|"$)/g,
+    ""
+  ),
+  projectId: JSON.stringify(window.env.REACT_APP_FIREBASE_PROJECT_ID).replace(
+    /(^"|"$)/g,
+    ""
+  ),
+  messagingSenderId: JSON.stringify(
+    window.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
+  ).replace(/(^"|"$)/g, ""),
+  appId: JSON.stringify(window.env.REACT_APP_FIREBASE_APP_ID).replace(
+    /(^"|"$)/g,
+    ""
+  ),
 };
 
 const app = initializeApp(firebaseConfig);
