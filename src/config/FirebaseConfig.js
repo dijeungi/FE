c1c0eee3ec5723c99c86389d13b2e1200841eb63
Heeyeon An <gmldus4613@naver.com>
2025-02-28 20:42:19 +0900

    firebase 수정

--- src/config/FirebaseConfig.js
+++ src/config/FirebaseConfig.js
@@ -2,11 +2,11 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: `${window.env.REACT_APP_FIREBASE_API_KEY}`,
-  authDomain: `${window.env.REACT_APP_FIREBASE_AUTH_DOMAIN}`,
-  projectId: `${window.env.REACT_APP_FIREBASE_PROJECT_ID}`,
-  messagingSenderId: `${window.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID}`,
-  appId: `${window.env.REACT_APP_FIREBASE_APP_ID}`,
+  apiKey: String(window.env.REACT_APP_FIREBASE_API_KEY),
+  authDomain: String(window.env.REACT_APP_FIREBASE_AUTH_DOMAIN),
+  projectId: String(window.env.REACT_APP_FIREBASE_PROJECT_ID),
+  messagingSenderId: String(window.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID),
+  appId: String(window.env.REACT_APP_FIREBASE_APP_ID),
 };
 
 const app = initializeApp(firebaseConfig);
