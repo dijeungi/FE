0a63ca11914ab321da48df78b39e81681322ad55
dijeungi <dijeungi@naver.com>
2025-03-01 21:33:47 +0900

    save

--- src/api/GoogleApi.js
+++ src/api/GoogleApi.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 import { API_SERVER_HOST, FRONT_HOST } from "../config/ApiConfig";
 
-// const rest_api_key = process.env.REACT_APP_GOOGLE_REST_API_KEY;
+// const rest_api_key = window.env.REACT_APP_GOOGLE_REST_API_KEY;
 const client_id = window.env.REACT_APP_GOOGLE_CLIENT_ID;
 const client_secret = window.env.REACT_APP_GOOGLE_CLIENT_SECRET;
 
@@ -24,45 +24,40 @@ console.log("REDIRECT URI:", redirect_uri);
 
 // 카카오 로그인 요청 -> 카카오 로그인 페이지(리다이렉트 페이지)로 이동
 export const getGoogleLoginLink = () => {
-  const googleURL = new URL(auth_code_path);
-  googleURL.searchParams.append("client_id", client_id);
-  googleURL.searchParams.append("redirect_uri", redirect_uri);
-  googleURL.searchParams.append("response_type", "code");
-  googleURL.searchParams.append("scope", "openid profile email");
-  googleURL.searchParams.append("state", "RANDOM_STRING");
+    const googleURL = new URL(auth_code_path);
+    googleURL.searchParams.append("client_id", client_id);
+    googleURL.searchParams.append("redirect_uri", redirect_uri);
+    googleURL.searchParams.append("response_type", "code");
+    googleURL.searchParams.append("scope", "openid profile email");
+    googleURL.searchParams.append("state", "RANDOM_STRING");
 
-  console.log("getGoogleLoginLink googleURL: ", googleURL.toString());
-  return googleURL.toString();
+    console.log("getGoogleLoginLink googleURL: ", googleURL.toString());
+    return googleURL.toString();
 };
 
 // 카카오 로그인 후 code를 받고, 서버가 대신 google oauth2서버에 access_token 요청
 export const getAccessToken = async (authCode) => {
-  try {
-    const res = await axios.get(
-      `${API_SERVER_HOST}/api/member/google/token?code=${authCode}`
-    );
-    console.log("getAccessToken res: ", res);
-    return res.data;
-  } catch (error) {
-    console.error("Error fetching access token:", error);
-    throw error;
-  }
+    try {
+        const res = await axios.get(`${API_SERVER_HOST}/api/member/google/token?code=${authCode}`);
+        console.log("getAccessToken res: ", res);
+        return res.data;
+    } catch (error) {
+        console.error("Error fetching access token:", error);
+        throw error;
+    }
 };
 
 // 카카오 사용자 정보 서버에 요청
 export const getMemberWithAccessToken = async (accessToken) => {
-  const headers = {
-    withCredentials: true,
-  };
-  try {
-    const res = await axios.get(
-      `${API_SERVER_HOST}/api/member/google?accessToken=${accessToken}`,
-      headers
-    );
-    console.log("getMemberWithAccessToken res: ", res);
-    return res.data;
-  } catch (error) {
-    console.error("Error fetching member data:", error);
-    throw error;
-  }
+    const headers = {
+        withCredentials: true,
+    };
+    try {
+        const res = await axios.get(`${API_SERVER_HOST}/api/member/google?accessToken=${accessToken}`, headers);
+        console.log("getMemberWithAccessToken res: ", res);
+        return res.data;
+    } catch (error) {
+        console.error("Error fetching member data:", error);
+        throw error;
+    }
 };
