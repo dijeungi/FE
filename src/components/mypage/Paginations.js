fe547e5a89dd638761a41a767b084b2e73d89c76
Heeyeon An <gmldus4613@naver.com>
2025-03-02 16:47:17 +0900

    마이페이지 예매내역 수정

--- src/components/mypage/Paginations.js
+++ src/components/mypage/Paginations.js
@@ -1,29 +1,29 @@
 import { Pagination, Stack } from "@mui/material";
 
 const Paginations = ({ page, totalPages, handlePageChange }) => {
-    return (
-        // Stack 컴포넌트를 사용하여 페이지네이션 컴포넌트를 수직 중앙 정렬하고, 위아래 여백을 추가합니다.
-        <Stack spacing={2} alignItems="center" sx={{ mt: 3, mb: 3 }}>
-            <Pagination
-                count={totalPages}
-                page={page}
-                onChange={handlePageChange}
-                color="secondary"
-                size="large"
-                sx={{
-                    "& .MuiPaginationItem-root": {
-                        color: "#2A0934",
-                        "&.Mui-selected": {
-                            backgroundColor: "#FFB7F2",
-                            "&:hover": {
-                                backgroundColor: "#FF9EE8",
-                            },
-                        },
-                    },
-                }}
-            />
-        </Stack>
-    );
+  return (
+    // Stack 컴포넌트를 사용하여 페이지네이션 컴포넌트를 수직 중앙 정렬하고, 위아래 여백을 추가합니다.
+    <Stack spacing={2} alignItems="center" sx={{ mt: 3, mb: 3 }}>
+      <Pagination
+        count={totalPages}
+        page={page}
+        onChange={handlePageChange}
+        color="secondary"
+        size="large"
+        sx={{
+          "& .MuiPaginationItem-root": {
+            color: "#2A0934",
+            "&.Mui-selected": {
+              backgroundColor: "#ffa48c ",
+              "&:hover": {
+                backgroundColor: "#ff8364",
+              },
+            },
+          },
+        }}
+      />
+    </Stack>
+  );
 };
 
 export default Paginations;
